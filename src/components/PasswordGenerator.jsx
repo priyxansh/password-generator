dbaf39eeff6d663d7b92312048a5c8be6d3d7abe
Priyansh <priyanshrai16@gmail.com>
2023-03-04 19:57:03 +0530

    Added settings with switches

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -2,16 +2,19 @@ import React, { useState, useEffect, useRef } from "react";
 import "./css/PasswordGenerator.css";
 
 export default function PasswordGenerator() {
+
+
     return (
         <section className="container">
             <h1>Password Generator</h1>
             <div className="display cell"></div>
             <LengthSlider />
+            <Settings />
         </section>
     );
 }
 
-const LengthSlider = (props) => {
+const LengthSlider = () => {
     const [length, setLength] = useState(10);
     const [maxLength, setMaxLength] = useState(24);
 
@@ -27,7 +30,7 @@ const LengthSlider = (props) => {
         return () => {
             inputRef.current.removeEventListener("wheel", inputScrollHandler);
         };
-    },[]);
+    }, []);
 
     return (
         <div className="length-wrapper">
@@ -48,4 +51,42 @@ const LengthSlider = (props) => {
             </div>
         </div>
     );
+};
+
+const Settings = () => {
+    return (
+        <div className="settings-wrapper">
+            <div className="options-container">
+                <div className="cell option">
+                    <span>Include Uppercase</span>
+                    <Switch defaultChecked={true} />
+                </div>
+                <div className="cell option">
+                    <span>Include Lowercase</span>
+                    <Switch defaultChecked={true} />
+                </div>
+                <div className="cell option">
+                    <span>Include Numbers</span>
+                    <Switch />
+                </div>
+                <div className="cell option">
+                    <span>Include Symbols</span>
+                    <Switch />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const Switch = (props) => {
+    return (
+        <label className="switch">
+            <input type="checkbox" defaultChecked={props.defaultChecked} />
+            <span className="switch-slider"></span>
+        </label>
+    );
+};
+
+Switch.defaultProps = {
+    defaultChecked: false,
 };
