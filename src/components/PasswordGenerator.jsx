500e8cd4c0c9feb27a21c22751ff9b19a6c051ae
Priyansh <priyanshrai16@gmail.com>
2023-03-05 01:34:46 +0530

    Lifted States Up And Refactored The Code

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -2,18 +2,48 @@ import React, { useState, useEffect, useRef } from "react";
 import "./css/PasswordGenerator.css";
 
 export default function PasswordGenerator() {
+    const [length, setLength] = useState(10);
+    const [options, setOptions] = useState([
+        { title: "Uppercase", checked: true, id: 0 },
+        { title: "Lowercase", checked: true, id: 1 },
+        { title: "Numbers", checked: false, id: 2 },
+        { title: "Symbols", checked: false, id: 3 },
+    ]);
+
+    const sliderChangeHandler = (e) => {
+        setLength(e.target.value);
+    };
+
+    const switchToggleHandler = (id) => {
+        const tempOptions = [...options];
+        const temp = tempOptions.find((option) => option.id === id);
+        temp.checked = !temp.checked;
+        setOptions(tempOptions);
+        console.log(options);
+    };
+
     return (
         <section className="container">
             <h1>Password Generator</h1>
             <div className="display cell"></div>
-            <LengthSlider />
-            <Settings />
+            <LengthSlider length={length} onChange={sliderChangeHandler} />
+            <div className="settings-wrapper">
+                <div className="options-container">
+                    {options.map((option) => (
+                        <Option
+                            title={option.title}
+                            checked={option.checked}
+                            id={option.id}
+                            onChange={switchToggleHandler}
+                        />
+                    ))}
+                </div>
+            </div>
         </section>
     );
 }
 
-const LengthSlider = () => {
-    const [length, setLength] = useState(10);
+const LengthSlider = ({ length, onChange }) => {
     const [maxLength, setMaxLength] = useState(24);
 
     const inputRef = useRef();
@@ -43,7 +73,7 @@ const LengthSlider = () => {
                     min={6}
                     max={maxLength}
                     value={length}
-                    onChange={(e) => setLength(e.target.value)}
+                    onChange={(e) => onChange(e)}
                 />
                 <span>24</span>
             </div>
@@ -51,40 +81,20 @@ const LengthSlider = () => {
     );
 };
 
-const Settings = () => {
+const Option = ({ title, checked, id, onChange }) => {
     return (
-        <div className="settings-wrapper">
-            <div className="options-container">
-                <div className="cell option">
-                    <span>Include Uppercase</span>
-                    <Switch defaultChecked={true} />
-                </div>
-                <div className="cell option">
-                    <span>Include Lowercase</span>
-                    <Switch defaultChecked={true} />
-                </div>
-                <div className="cell option">
-                    <span>Include Numbers</span>
-                    <Switch />
-                </div>
-                <div className="cell option">
-                    <span>Include Symbols</span>
-                    <Switch />
-                </div>
-            </div>
+        <div className="cell option">
+            <span>Include {title}</span>
+            <label className="switch">
+                <input
+                    type="checkbox"
+                    defaultChecked={checked}
+                    onChange={() => {
+                        onChange(id);
+                    }}
+                />
+                <span className="switch-slider"></span>
+            </label>
         </div>
     );
-};
-
-const Switch = (props) => {
-    return (
-        <label className="switch">
-            <input type="checkbox" defaultChecked={props.defaultChecked} />
-            <span className="switch-slider"></span>
-        </label>
-    );
-};
-
-Switch.defaultProps = {
-    defaultChecked: false,
 };
