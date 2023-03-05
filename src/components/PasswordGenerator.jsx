6fecfeec7047631eb74bd8c4236f19d10e24f7d5
Priyansh <priyanshrai16@gmail.com>
2023-03-05 10:08:24 +0530

    Added logic to generate passwords

--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -3,11 +3,27 @@ import "./css/PasswordGenerator.css";
 
 export default function PasswordGenerator() {
     const [length, setLength] = useState(10);
+    const [password, setPassword] = useState("");
     const [options, setOptions] = useState([
-        { title: "Uppercase", checked: true, id: 0 },
-        { title: "Lowercase", checked: true, id: 1 },
-        { title: "Numbers", checked: false, id: 2 },
-        { title: "Symbols", checked: false, id: 3 },
+        {
+            title: "Uppercase",
+            checked: true,
+            id: 0,
+            template: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+        },
+        {
+            title: "Lowercase",
+            checked: true,
+            id: 1,
+            template: "abcdefghijklmnopqrstuvwxyz",
+        },
+        { title: "Numbers", checked: false, id: 2, template: "0123456789" },
+        {
+            title: "Symbols",
+            checked: false,
+            id: 3,
+            template: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+        },
     ]);
 
     const sliderChangeHandler = (e) => {
@@ -19,13 +35,32 @@ export default function PasswordGenerator() {
         const temp = tempOptions.find((option) => option.id === id);
         temp.checked = !temp.checked;
         setOptions(tempOptions);
-        console.log(options);
     };
+
+    useEffect(() => {
+        let template = "";
+        let tempPassword = "";
+
+        options.forEach((option) => {
+            if (option.checked) {
+                template += option.template;
+            }
+        });
+
+        for (let i = 0; i < length; i++) {
+            tempPassword +=
+                template[Math.floor(Math.random() * template.length)];
+        }
+
+        setPassword(tempPassword);
+    }, [length, options]);
 
     return (
         <section className="container">
             <h1>Password Generator</h1>
-            <div className="display cell"></div>
+            <div className="display cell">
+                <span className="password">{password}</span>
+            </div>
             <LengthSlider length={length} onChange={sliderChangeHandler} />
             <div className="settings-wrapper">
                 <div className="options-container">
@@ -45,20 +80,20 @@ export default function PasswordGenerator() {
 
 const LengthSlider = ({ length, onChange }) => {
     const [maxLength, setMaxLength] = useState(24);
-
-    const inputRef = useRef();
+    const inputRef = useRef("");
 
     const inputScrollHandler = (e) => {
         e.preventDefault();
     };
 
     useEffect(() => {
+        // let element = inputRef.current
         inputRef.current.addEventListener("wheel", inputScrollHandler);
 
         return () => {
             inputRef.current.removeEventListener("wheel", inputScrollHandler);
         };
-    }, []);
+    });
 
     return (
         <div className="length-wrapper">
